99a0370963a5ab984bc565f63c8d6c6e3c3a3dbb
Tony Okonski <tonyokonski@gmail.com>
2015-01-09 12:31:30 -0600

    Alot. Further Mobile first.
    Added button to toggle menu.

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,19 +1,40 @@
-window.onload = function () {
-	var collapse = document.getElementById('navCollapse');
-	var nav = document.getElementById('nav');
-	//toggle class utility function
-	function classToggle(element, tclass) {
-		var classes = element.className,
+$(document).ready(function() {
+
+	$('#menu-trigger').click(function() {
+
+		$('#nav').slideToggle();
+
+	});
+
+});
+
+
+/*var Utils = {
+	var classToggle = function(element, tclass) { //toggle class utility function
+			var classes = element.className,
 			pattern = new RegExp(tclass);
-		var hasClass = pattern.test(classes);
-		//toggle the class
-		classes = hasClass ? classes.replace(pattern, '') :	classes + ' ' + tclass;
-		element.className = classes.trim();
-	};
-	classToggle(nav, 'hide');
-	classToggle(collapse, 'active');
-	collapse.onclick = function() {
-		classToggle(nav, 'hide');
-		return false;
-	};
+			var hasClass = pattern.test(classes);
+			//toggle the class
+			classes = hasClass ? classes.replace(pattern, '') :	classes + ' ' + tclass;
+			element.className = classes.trim();
+		};
 }
+
+window.onload = function () {
+	var nav = document.getElementById('nav');
+	var navItem = nav.getElementsByTagName('li');
+	//is it floated?
+	var floated = navItem[0].currentStyle ? el.currentStyle['float'] : document.defaultView.getComputedStyle(navItem[0],null).getPropertyValue('float');
+	
+	if (floated != 'left') {
+		var collapse = document.getElementById('nav-collapse');
+
+		Utils.classToggle(nav, 'hide');
+		Utils.classToggle(collapse, 'active');
+
+		collapse.onclick = function() {
+			classToggle(nav, 'hide');
+			return false;
+		};
+	}
+}*/
