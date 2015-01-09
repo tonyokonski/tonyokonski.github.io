ce973ab8a8601eb0218c6a2d0ee005d39253b3f2
Tony Okonski <tonyokonski@gmail.com>
2015-01-09 12:36:31 -0600

    toggle.js
    trimmed the fat

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -7,34 +7,3 @@ $(document).ready(function() {
 	});
 
 });
-
-
-/*var Utils = {
-	var classToggle = function(element, tclass) { //toggle class utility function
-			var classes = element.className,
-			pattern = new RegExp(tclass);
-			var hasClass = pattern.test(classes);
-			//toggle the class
-			classes = hasClass ? classes.replace(pattern, '') :	classes + ' ' + tclass;
-			element.className = classes.trim();
-		};
-}
-
-window.onload = function () {
-	var nav = document.getElementById('nav');
-	var navItem = nav.getElementsByTagName('li');
-	//is it floated?
-	var floated = navItem[0].currentStyle ? el.currentStyle['float'] : document.defaultView.getComputedStyle(navItem[0],null).getPropertyValue('float');
-	
-	if (floated != 'left') {
-		var collapse = document.getElementById('nav-collapse');
-
-		Utils.classToggle(nav, 'hide');
-		Utils.classToggle(collapse, 'active');
-
-		collapse.onclick = function() {
-			classToggle(nav, 'hide');
-			return false;
-		};
-	}
-}*/
