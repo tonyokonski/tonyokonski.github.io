67dcf3b58ca0d87abd263a92c2cbfdd136ba5044
Tony Okonski <tonyokonski@gmail.com>
2015-01-16 09:20:50 -0600

    Possible menu fix.
    updated for possible menu fix found on
    https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c

--- README.txt
+++ README.txt
@@ -1,10 +1,14 @@
-Hello! This is my online resume. As you can tell it is a work in progress. I want to create a baseline experience, and then implement presentation and interactivity in a way that preserves the baseline, but provides a richer experience for those who are using modern browsing technology.
+Hello! This is my online resume. As you can tell it is a work in progress, and I imagine that it always will be.
 
-Needs:
+Design Philosophy: Mobile first with progressive enhancement. I want to create a simple baseline experience, and then design presentation and interactivity in a way that provides a richer experience for those who are using modern browsing technology.
 
-1. Styling. So much styling.
+Methodology: Free flow. A few sketches with no mock-ups or prototyping, and in my opinion, a bad design experience. I felt I am spending large amounts of time fixing many small issues without resolving major ones. Further details about this site's design and construction will be availible on a design description page in the near future. In future projects I will have a more structured approach to design and implementation.
 
-2. Upload portfolio examples.
+Needs (as of 1/16/15):
+
+1. Finish layout for larger viewports.
+
+2. Upload portfolio examples with their design description pages.
 
 3. Develop the user experience.
 
--- scripts/toggle.js
+++ scripts/toggle.js
@@ -42,12 +42,45 @@ $(document).ready(function() {
 
 	});
 
-	//menu hide on scroll attempt. Need a conditional to constrain it to mobile viewports.
+	//menu hide on scroll attempt. Solution found at https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c. 
 
-	/*$(window).scroll(function() {
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('header').outerHeight();
 
-		$('header').css("top", "-50").fadeOut('slow');
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
 
-	});*/
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('header').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $('header').removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
 
 });
